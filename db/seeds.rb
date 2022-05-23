58a5f1449ae4f52920975f0759db67a58139c0b8
Timothee Peroulakis <timperoulakis@gmail.com>
2022-05-23 13:58:26 +0200

    refactored

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,11 +23,8 @@ hobbits = [
 ]
 
 hobbits.each do |hobbit|
-  user = User.new(
-    first_name: hobbit[:first_name],
-    last_name: hobbit[:last_name],
-    password: "secret"
-  )
+  user = User.new(hobbit)
+  user.password = "secret"
   user.email = Faker::Internet.email(name: user.first_name, domain: user.last_name)
   user.save
 end
