2274f2d18a81a132c1fcebfe60874c2538902a05
Bastien Grandjean <bastien.grandjean.75@gmail.com>
2022-05-23 12:59:16 +0200

    routes and controller created

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :offers do
+    resources :reservations, only: %i[new create]
+  end
+  resources :reservations, only: %i[edit update destroy show index]
   # Defines the root path route ("/")
   # root "articles#index"
 end
