35dadf1cb052f7ccf43e82765a721881425e60e0
Timothee Peroulakis <timperoulakis@gmail.com>
2022-05-23 17:04:38 +0200

    removed unnecessary logs

--- app/javascript/controllers/total_price_controller.js
+++ app/javascript/controllers/total_price_controller.js
@@ -5,13 +5,10 @@ export default class extends Controller {
   static targets = ["start", "end", "total", "daily"];
 
   connect() {
-    console.log("connected");
-    console.log(this.element);
   }
   calc() {
     let startDate = Date.parse(this.startTarget.value);
     let endDate = Date.parse(this.endTarget.value);
-    console.log(endDate - startDate);
     this.totalTarget.innerHTML = this.dailyTarget.innerHTML * (endDate - startDate) / 86400000;
   }
 }
