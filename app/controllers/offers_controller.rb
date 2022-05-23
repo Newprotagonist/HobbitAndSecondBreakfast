c9629089fa5f4c9186eb8295463711ed7c65a079
Dagmara Korta <dagmara.korta@gmail.com>
2022-05-23 14:56:17 +0200

    Added offer - new, create and edit, update, as well as their respective html.erb files

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -2,4 +2,38 @@ class OffersController < ApplicationController
   def show
     @offer = Offer.find(params[:id])
   end
+
+  def new
+    @offer = Offer.new
+  end
+
+  def create
+    @offer = Offer.new(params_require)
+    if @offer.save
+      redirect_to offer_path(@offer)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @offer = Offer.find(params[:id])
+  end
+
+  def updated
+    @offer = Task.find(params[:id])
+    @offer.update(params_require)
+    if @offer.save
+      redirect_to offer_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def params_require
+    params.require(:offer).permit(:localisation, :price, :summary,)
+  end
+
 end
