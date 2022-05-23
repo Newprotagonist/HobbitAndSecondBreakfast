81199d81926aa8f0c30d380266e06b507afffec0
Vincent Ramos <vince9322@gmail.com>
2022-05-23 12:44:21 +0200

    reservation model created

---
+++ db/migrate/20220523104341_create_reservations.rb
@@ -0,0 +1,13 @@
+class CreateReservations < ActiveRecord::Migration[7.0]
+  def change
+    create_table :reservations do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :offer, null: false, foreign_key: true
+      t.date :start_date
+      t.date :end_date
+      t.float :total_price
+
+      t.timestamps
+    end
+  end
+end
