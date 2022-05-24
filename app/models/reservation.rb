2c701bfd667514e514a4c0c3b015782a08a00701
Vincent Ramos <vince9322@gmail.com>
2022-05-24 14:06:12 +0200

    added date check on reservation#new form

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,17 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :offer
-  validates :end_date, presence: true, date: { after_or_equal_to: :start_date }
+  validates :end_date, presence: true
   validates :start_date, presence: true
+  validate :end_date_is_after_start_date
+
+  private
+
+  def end_date_is_after_start_date
+    return if end_date.blank? || start_date.blank?
+
+    if end_date < start_date
+      errors.add(:end_date, "cannot be before the start date")
+    end
+  end
 end
