9ded6dc73479c3c5e11fa50b5363a235e974f351
Timothee Peroulakis <timperoulakis@gmail.com>
2022-05-24 13:47:30 +0200

    working

--- app/policies/reservation_policy.rb
+++ app/policies/reservation_policy.rb
@@ -2,7 +2,11 @@ class ReservationPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
     def resolve
-      scope.where(user: user)
+      if user.hobbit
+        scope.joins("JOIN offers ON offers.id = reservations.offer_id WHERE offers.user_id = #{user.id}")
+      else
+        scope.where(user: user)
+      end
     end
   end
 
@@ -15,7 +19,7 @@ class ReservationPolicy < ApplicationPolicy
   end
 
   def show?
-    record.user == user
+    record.user == user or record.offer.user == user
   end
 
   def edit?
