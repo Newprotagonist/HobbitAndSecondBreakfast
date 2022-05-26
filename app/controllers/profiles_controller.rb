456417337c04562ba8c22e12711c19cb8cb0bfbc
Dagmara Korta <dagmara.korta@gmail.com>
2022-05-26 15:17:19 +0200

    Finished profile page, with uploading pic option

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,16 +1,11 @@
 class ProfilesController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[profile]
   before_action :set_user
+
   def profile
     skip_authorization
-
   end
 
   private
-
-  def users_params
-    params.require(:user).permit(:photo)
-  end
 
   def set_user
     @user = current_user
