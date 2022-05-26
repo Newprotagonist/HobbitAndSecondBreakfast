a0e4d930f56ec264552ed65d594c26290490196d
Timothee Peroulakis <timperoulakis@gmail.com>
2022-05-26 17:26:53 +0200

    seed

--- db/seeds/production.rb
+++ db/seeds/production.rb
@@ -1,12 +1,20 @@
+require "faker"
+
+# create hobbit users and their respective offers
 User.all.each do |hobbit|
+  summary = ""
+  10.times do
+    summary << "#{Faker::Fantasy::Tolkien.poem}\n"
+  end
   Offer.create!(
-    location: "The Shire",
+    location: Faker::Movies::LordOfTheRings.location,
     price: rand(75..120),
-    summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+    summary: summary,
     user: hobbit
   )
 end
 
+# create non-hobbit user
 gandalf = User.create(
   first_name: "Gandalf",
   last_name: "The White",
@@ -15,13 +23,71 @@ gandalf = User.create(
   hobbit: false
 )
 
+# create reservations
 Offer.all.each do |offer|
   Reservation.create(
     title: "Quest to destroy the Ring",
     offer: offer,
     user: gandalf,
-    start_date: Date.today,
-    end_date: Date.today + 20,
-    total_price: offer.price * 20
+    start_date: Date.parse("29 July 1954"),
+    end_date: Date.parse("20 October 1955"),
+    total_price: offer.price * 20,
+    status: "Accepted"
   )
 end
+
+# review all past reservations
+Reservation.all.each do |reservation|
+  hobbit = reservation.offer.user
+  wizard = reservation.user
+  Review.create!(
+    giver: hobbit,
+    receiver: wizard,
+    reservation: reservation,
+    content: "% 10s" % Faker::Fantasy::Tolkien.poem,
+    rating: rand(0..5.0).round(1)
+  )
+  Review.create!(
+    giver: wizard,
+    receiver: hobbit,
+    reservation: reservation,
+    content: "% 10s" % Faker::Fantasy::Tolkien.poem,
+    rating: rand(0..5.0).round(1)
+  )
+  reservation.update!(status: "Done")
+end
+
+# create non-reviewed but reviewable reservations
+
+Offer.all.sample(3).each do |offer|
+  reservation = Reservation.new(
+    title: "Quest to eat the third breakfast",
+    offer: offer,
+    user: gandalf,
+    start_date: Date.today - 7,
+    end_date: Date.today - 2,
+    total_price: offer.price * 5,
+    status: "Accepted"
+  )
+  reservation.save(validate: false)
+end
+
+# create non-reviewed reservations
+Offer.all.sample(3).each do |offer|
+  Reservation.create(
+    title: "Quest to eat the third breakfast",
+    offer: offer,
+    user: gandalf,
+    start_date: Date.today - 1,
+    end_date: Date.today + 9,
+    total_price: offer.price * 10
+  )
+end
+
+User.create(
+  first_name: "Saruman",
+  last_name: "The Betrayer",
+  email: "saruman@istari.org",
+  password: "secret",
+  hobbit: false
+)
