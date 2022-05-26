4de5516ed816ce27f159c3d7f41deb1689da3413
Dagmara Korta <dagmara.korta@gmail.com>
2022-05-26 11:22:38 +0200

    Created review model and reviews controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_24_113541) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_26_090856) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,6 +65,17 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_24_113541) do
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.float "rating"
+    t.bigint "user_id", null: false
+    t.bigint "offer_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["offer_id"], name: "index_reviews_on_offer_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -85,4 +96,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_24_113541) do
   add_foreign_key "offers", "users"
   add_foreign_key "reservations", "offers"
   add_foreign_key "reservations", "users"
+  add_foreign_key "reviews", "offers"
+  add_foreign_key "reviews", "users"
 end
