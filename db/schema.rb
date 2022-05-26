2d5e961b0af7f775c57a6cb48556791cf263e64e
Bastien Grandjean <bastien.grandjean.75@gmail.com>
2022-05-26 17:20:45 +0200

    pushing to heroku

--- db/schema.rb
+++ db/schema.rb
@@ -52,6 +52,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_26_112044) do
     t.index ["user_id"], name: "index_offers_on_user_id"
   end
 
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text "content"
+    t.string "searchable_type"
+    t.bigint "searchable_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable"
+  end
+
   create_table "reservations", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "offer_id", null: false
