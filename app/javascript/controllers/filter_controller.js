be40a78a9c783e05fd83456059174dad49779847
Bastien Grandjean <bastien.grandjean.75@gmail.com>
2022-05-26 15:15:44 +0200

    change for tim </3

--- app/javascript/controllers/filter_controller.js
+++ app/javascript/controllers/filter_controller.js
@@ -4,12 +4,10 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ['price', 'value']
   connect() {
-    console.log("Hello Bastien, you're very good at Super Smash")
     this.priceTarget.value = 0
   }
 
   display() {
-    console.log(this.priceTarget.value);
     this.valueTarget.innerText = `${this.priceTarget.value}`
   }
 }
