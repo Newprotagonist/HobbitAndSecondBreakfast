a57f0304ec35386edf2951a334eb6a7bcdc61645
Timothee Peroulakis <timperoulakis@gmail.com>
2022-05-26 12:11:50 +0200

    clean

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_one :offer
   has_many :reservations
-  has_many :reviews
+  has_many :reviews_as_receiver, class_name: "Review", foreign_key: :receiver_id
+  has_many :reviews_as_giver, class_name: "Review", foreign_key: :giver_id
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :first_name, presence: true, uniqueness: { scope: :last_name }
