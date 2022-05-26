a4cc62b5c341fa5f0e6b24085cb7de60bea67bab
Dagmara Korta <dagmara.korta@gmail.com>
2022-05-26 16:44:54 +0200

    Alerts done

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -4,5 +4,5 @@ class Offer < ApplicationRecord
   validates :price, presence: true, numericality: { greater_than: 0 }
   validates :location, presence: true
   validates :summary, presence: true, length: { minimum: 32, maximum: 4096 }
-  has_many_attached :photos
+  has_many_attached :photos, dependent: :purge_later
 end
